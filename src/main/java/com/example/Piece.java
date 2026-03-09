74284b958f2c610c123937a27227507a07070b0e
kaos101-max <watanabedaisuke616@gmail.com>
2026-03-09 15:46:34 +0000

    3/9/26 (1)

--- src/main/java/com/example/Piece.java
+++ src/main/java/com/example/Piece.java
@@ -1,4 +1,5 @@
 package com.example;
+
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -14,70 +15,84 @@ import javax.imageio.ImageIO;
 public class Piece {
     private final boolean color;
     private BufferedImage img;
-    
+
     public Piece(boolean isWhite, String img_file) {
         this.color = isWhite;
-         
+
         try {
             if (this.img == null) {
-                this.img = ImageIO.read(new File(System.getProperty("user.dir")+img_file));
+                this.img = ImageIO.read(new File(System.getProperty("user.dir") + img_file));
             }
-          } catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("File not found: " + e.getMessage());
-          }
+        }
     }
-    
-    
 
-    
     public boolean getColor() {
         return color;
     }
-    
+
     public Image getImage() {
         return img;
     }
-    
+
     public void draw(Graphics g, Square currentSquare) {
         int x = currentSquare.getX();
         int y = currentSquare.getY();
-        
+
         g.drawImage(this.img, x, y, null);
     }
-    
-    
-    // TO BE IMPLEMENTED!
-    //return a list of every square that is "controlled" by this piece. A square is controlled
-    //if the piece capture into it legally.
-    public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
-     return null;
-    }
-    
 
-    //TO BE IMPLEMENTED!
-    //implement the move function here
-    //it's up to you how the piece moves, but at the very least the rules should be logical and it should never move off the board!
-    //returns an arraylist of squares which are legal to move to
-    //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
-    //going to score any points.
-    public ArrayList<Square> getLegalMoves(Board b, Square start){
+    // TO BE IMPLEMENTED!
+    // return a list of every square that is "controlled" by this piece. A square is
+    // controlled
+    // if the piece capture into it legally.
+    public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
+        return null;
+    }
+
+    // TO BE IMPLEMENTED!
+    // implement the move function here
+    // it's up to you how the piece moves, but at the very least the rules should be
+    // logical and it should never move off the board!
+    // returns an arraylist of squares which are legal to move to
+    // please note that your piece must have some sort of logic. Just being able to
+    // move to every square on the board is not
+    // going to score any points.
+    public ArrayList<Square> getLegalMoves(Board b, Square start) {
         ArrayList<Square> moves = new ArrayList<>();
 
-        //the board in which you can move
-        Square [][] board = b.getSquareArray();
+        // the board in which you can move
+        Square[][] board = b.getSquareArray();
 
-        //each move is up (or down if we're black) one row and either left or right
+        // each move is up (or down if we're black) one row and either left or right
 
-    	int startX = start.getCol();
+        int startX = start.getCol();
         int startY = start.getRow();
         int leftRight = 1;
+        //The custom chess piece has its uniqueness and limitations. 
+        // It moves stiraght and goes right and left like a zigzag, and it can only be moved on the two middle rows of the board.
+        if (color) {
+            for (int i = startY - 1; i >= 0; i--) {
+                startX += leftRight;
+                if (startX + leftRight >= 8 && startX + leftRight < 0) {
+                    throw new IndexOutOfBoundsException("we're never supposed to be on the edge!");
+                }
+                if (!board[i][startX].isOccupied() || board[i][startX].getOccupyingPiece().getColor() != color) {
+                    moves.add(board[i][startX]);
+                } else {
+                    break;
+                }
+                if (leftRight == 1){
+                    leftRight = -1;
+                }
+                else{
+                    leftRight = 1;
+                }
+            }
 
-        for(int i=startY-1; i>=0; i--){
-          
         }
-        
-
-        Square move = board[startY-1][startX+1];
+        Square move = board[startY - 1][startX + 1];
         moves.add(move);
 
         return moves;
